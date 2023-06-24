5f7761b3dec28fb44dcc9ff800e41778f7783f2a
vankenobi <musakucuk99@gmail.com>
2023-06-25 00:41:27 +0300

    Observer pattern is added.

---
+++ ObserverPattern/Amazon.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace ObserverPattern
+{
+	public class Amazon
+	{
+		
+		private Dictionary<IObserver,Product> observers = new();
+
+		public void Register(IObserver observer,Product product)
+		{
+			observers.TryAdd(observer, product);
+		}
+		
+		public void UnRegister(IObserver observer)
+		{
+			observers.Remove(observer);
+		}
+
+		public void NotifyAll()
+		{
+			foreach (var kv in observers)
+			{
+				kv.Key.StockUpdate(kv.Value);
+			}
+		}
+
+		public void NotifyForProductName(string productName)
+		{
+			foreach (var kv in observers)
+			{
+				if (kv.Value.Name == productName)
+				{
+					kv.Key.StockUpdate(kv.Value);
+				}
+			}
+		}
+	}
+
+	public interface IObserver
+	{
+		void StockUpdate(Product product);
+	}
+
+	
+	public class MusaObserver : IObserver
+	{
+		public string FullName { get; set; }
+
+		public MusaObserver(string fullName)
+		{
+			FullName = fullName ?? throw new ArgumentNullException(nameof(fullName));
+		}
+
+        public void StockUpdate(Product product)
+        {
+			Console.WriteLine($"{FullName}, {product.Name} in stock !! ");
+        }
+    }
+
+    public class JackObserver : IObserver
+    {
+        public string FullName { get; set; }
+
+        public JackObserver(string fullName)
+        {
+            FullName = fullName ?? throw new ArgumentNullException(nameof(fullName));
+        }
+
+        public void StockUpdate(Product product)
+        {
+            Console.WriteLine($"{FullName}, {product.Name} in stock !! ");
+        }
+    }
+
+
+    public class Product
+	{
+		public string Name { get; set; }
+		public decimal Price { get; set; }
+
+		public Product(string name, decimal price)
+		{
+			Name = name;
+			Price = price;	
+		}
+	}
+}
+
---
+++ ObserverPattern/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ObserverPattern;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //new Products
+            var appleWatch = new Product("Apple Watch Series 8",12000);
+            var iphone = new Product("Iphone14", 550000);
+
+            //Observer
+            var musaObserver = new MusaObserver("Musa Kucuk");
+            var jackObserver = new JackObserver("Hugh Jackman");
+
+            Amazon amazon = new Amazon();
+
+            amazon.Register(musaObserver, appleWatch);
+            amazon.Register(jackObserver, iphone);
+            amazon.Register(jackObserver, appleWatch);
+
+            // Send notification to all observer
+            amazon.NotifyAll();
+
+            // Send a notification for specific product
+            //amazon.NotifyForProductName("Iphone14");
+
+           
+            
+            
+        }
+    }
+}
